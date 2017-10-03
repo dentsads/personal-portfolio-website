63e9765ea328622daa737d4f47eeff3565a9a9a9
Dimi <dentsads@ubuntu.ubuntu-domain>
2017-10-03 19:51:39 +0200

    readded aws_access_key to s3cmd call

--- deploy.sh
+++ deploy.sh
@@ -18,5 +18,6 @@ fi
 S3BUCKET="dentsads-personal-portfolio-site"
 
 echo 'Syncing files to S3 bucket'
-s3cmd sync src/ s3://$S3BUCKET/ --access_key= --secret_key=$AWS_SECRET_KEY -P --no-mime-magic -r
+s3cmd sync src/ s3://$S3BUCKET/ --access_key=$AWS_ACCESS_KEY --secret_key=$AWS_SECRET_KEY -P --no-mime-magic -r && echo "Syncing was successful"
+
 exit $?
