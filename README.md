54dd5d76814b769055f78fc1ec72f56f979edf01
Dimi <dentsads@ubuntu.ubuntu-domain>
2017-10-03 19:49:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# dentsas.com
+
+[Install s3cmd](http://s3tools.org/s3cmd) to build the site.
+
+`sudo apt-get install s3cmd`
+
+## Local testing
+
+Open `src/index.html` to see the site.
+
+## Deploy to S3
+
+Set the required environment variables for the [AWS access keys](https://console.aws.amazon.com/iam/home#/security_credential)
+
+```
+export AWS_ACCESS_KEY=<aws-access-key>
+export AWS_SECRET_KEY=<aws-secret-key>
+```
+
+Deploy by syncing to S3
+
+`sh deploy.sh`
---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$AWS_ACCESS_KEY" ]; then
+    echo "Need to set the AWS_ACCESS_KEY environment variable for the AWS access token"
+    exit 1
+fi
+
+if [ -z "$AWS_SECRET_KEY" ]; then
+    echo "Need to set the AWS_SECRET_KEY environment variable for the AWS secret token"
+    exit 1
+fi
+
+if ! which s3cmd > /dev/null; then
+   echo "s3cmd package was not found. Please install it."
+   exit 1
+fi
+
+S3BUCKET="dentsads-personal-portfolio-site"
+
+echo 'Syncing files to S3 bucket'
+s3cmd sync src/ s3://$S3BUCKET/ --access_key= --secret_key=$AWS_SECRET_KEY -P --no-mime-magic -r
+exit $?
