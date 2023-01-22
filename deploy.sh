096afd1d39f2ea7d04fbc29e619b538aceb9d21c
d.dentsas <d.dentsas@googlemail.com>
2023-01-22 13:14:30 +0100

    change target S3 bucket to enable direct CNAME resolution

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ if ! which s3cmd > /dev/null; then
    exit 1
 fi
 
-S3BUCKET="dentsads-personal-portfolio-site"
+S3BUCKET="dimitrios.dentsas.com"
 
 echo 'Syncing files to S3 bucket'
 s3cmd sync src/ s3://$S3BUCKET/ --access_key=$AWS_ACCESS_KEY --secret_key=$AWS_SECRET_KEY -P --no-mime-magic -r && echo "Syncing was successful"
